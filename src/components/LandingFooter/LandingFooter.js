e00fe743dab943e686e117181b7a870245a534f5
Nika Otiashvili <nikaoto@gmail.com>
2018-01-24 16:29:02 +0400

    add LandingFooter component

---
+++ src/components/LandingFooter/LandingFooter.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import "./LandingFooter.css"
+
+export default class LandingFooter extends Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {};
+	}
+
+	render() {
+		return(
+      <div className="landing-footer">
+			 <h1>FOOTER</h1>
+      </div>
+    )
+	}
+}
