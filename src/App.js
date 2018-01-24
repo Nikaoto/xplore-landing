b3f22a336582ea1090fa2b540d308b30dfac171b
Nika Otiashvili <nikaoto@gmail.com>
2018-01-24 16:29:17 +0400

    add language change func to header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import LandingHeader from "./components/LandingHeader"
 import LandingBody from "./components/LandingBody"
-/*import LandingFooter from "./components/LandingFooter"*/
+import LandingFooter from "./components/LandingFooter"
 import './App.css';
 
 class App extends Component {
@@ -10,21 +10,34 @@ class App extends Component {
     super(props);
   
     this.state = {language: "en"};
+    this.strings = {
+      ka: {
+        text: "ტექსტი", 
+      },
+      en: {
+        text: "Text"
+      },
+      es: {
+        text: "Texteroni"
+      }
+    }
+    this.onLanguageChange = this.onLanguageChange.bind(this)
   }
 
   onLanguageChange(newLanguage) {
-    console.log("App.js- new lang is", newLanguage)
-    //this.setState({language: newLanguage})
+    this.setState({language: newLanguage})
   }
 
   render() {
     const language = this.state.language
+    const strings = this.strings[language]
 
     return (
       <div className="landing-page">
+        <p>{strings.text}</p>
         <LandingHeader language={language} logo={logo} onLanguageChange={this.onLanguageChange}/>
         <LandingBody language={language}/>
-        {/*<LandingFooter language={language}/>*/}
+        <LandingFooter language={language}/>
       </div>
     );
   }
