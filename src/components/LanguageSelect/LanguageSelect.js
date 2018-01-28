49eac79423b44394ad1f8cbf16413e92dbaef533
Nika Otiashvili <nikaoto@gmail.com>
2018-01-29 03:16:42 +0400

    remove languageselect hardcodes

--- src/components/LanguageSelect/LanguageSelect.js
+++ src/components/LanguageSelect/LanguageSelect.js
@@ -3,10 +3,15 @@ import blank from "./blank.gif"
 import "./flags.css"
 import "./LanguageSelect.css"
 
+const languageNames = {
+  en: "English, US",
+  ka: "ქართული",
+  ru: "Русский",
+}
+
 export default class LanguageSelect extends Component {
   constructor(props) {
     super(props);
-    
     this.state = {buttonTextColor: "#b1b7bf", showList: false};
     this.onMouseEnter = this.onMouseEnter.bind(this)
     this.onMouseLeave = this.onMouseLeave.bind(this)
@@ -41,15 +46,15 @@ export default class LanguageSelect extends Component {
         <ul id="languageList" className="dropdown-content">
           <li className="dropdown-option" onClick={() => this.changeLanguage("en")}>
             <img src={blank} className="flag flag-us" alt="EN_US"/>
-            English, US
+            {languageNames.en}
           </li>
           <li className="dropdown-option" onClick={() => this.changeLanguage("ka")}>
             <img src={blank} className="flag flag-ge" alt="KA_GE"/>
-            ქართული
+            {languageNames.ka}
           </li>
           <li className="dropdown-option" onClick={() => this.changeLanguage("ru")}>
             <img src={blank} className="flag flag-ru" alt="RU"/>
-            Ruski
+            {languageNames.ru}
           </li>
         </ul>
       )
--- src/values/config.js
+++ src/values/config.js
@@ -1,6 +1,6 @@
 const config = {
   siteTitle: "Xplore - Plan Hikes, Find Hikers",
-  languages: ["en", "ka", "ru"],
+  languages: ["en", "ka", "ru"],//, "de", "fr", "it", "es"],
   defaultLanguage: "en",
 }
 
