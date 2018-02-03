2e28e633ae4e77727900679c53049ae22c004a74
Nika Otiashvili <nikaoto@gmail.com>
2018-02-04 02:21:22 +0400

    add social links

--- src/values/config.js
+++ src/values/config.js
@@ -2,6 +2,9 @@ const config = {
   siteTitle: "Xplore - Plan Hikes, Find Hikers",
   languages: ["en", "ka", "ru"],//, "de", "fr", "it", "es"],
   defaultLanguage: "en",
+  twitterLink: "https://twitter.com/Xplore86355464",
+  facebookLink: "https://facebook.com/xploreapp",
+  instagramLink: "https://instagram.com/xploreapp"
 }
 
 export default config
