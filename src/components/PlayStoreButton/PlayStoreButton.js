1b0b289d306f58774ff6dbf57d0b719c6183c42f
Nika Otiashvili <nikaoto@gmail.com>
2018-02-12 00:55:36 +0400

    add fade effects to androidButton

--- src/components/PlayStoreButton/PlayStoreButton.js
+++ src/components/PlayStoreButton/PlayStoreButton.js
@@ -1,21 +1,62 @@
 import React, { Component } from "react"
 import "./PlayStoreButton.css"
-import androidIcon from "./android.svg"
+import lightIcon from "./android.png"
+import darkIcon from "./android_dark.png"
+
+const animationTime = 100
 
 export default class PlayStoreButton extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = { androidIcon: lightIcon }
+    this.onMouseEnter = this.onMouseEnter.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+  }
+
+  onMouseEnter() {
+    setTimeout(() => {
+      this.setState({
+        androidIcon: darkIcon
+      })
+    }, animationTime)
+  }
+
+  onMouseLeave() {
+    setTimeout(() => {
+      this.setState({
+        androidIcon: lightIcon
+      })
+    }, animationTime)
   }
 
   render() {
+    const androidIcon = this.state.androidIcon
     return(
-      <a className="play-store-btn" href="#">
-        <img src={androidIcon}/>
-        <span>Get it on</span>
-        <span>Google Play</span>
+      <a className="play-store-btn" 
+        href="#" 
+        onMouseEnter={this.onMouseEnter} 
+        onMouseLeave={this.onMouseLeave} >
+        <img src={androidIcon} style={styles.icon}/>
+        <div style={styles.text}>
+          <span>Get it on</span>
+          <strong>Google Play</strong>
+        </div>
       </a>
     )
+  }
+}
+
+const styles = {
+  icon: {
+    width: 40,
+    height: 40,
+    marginRight: 10, 
+  },
+  text: { 
+    display: "flex", 
+    flexDirection: "column",
+    justifyContent: "center",
+    marginRight: 8,
   }
 }
