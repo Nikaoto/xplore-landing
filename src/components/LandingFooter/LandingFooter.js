6adbcb72a5812234cc318aba8156b76b1584df18
Nikaoto <nikaoto@gmail.com>
2018-02-23 17:59:00 +0400

    center footer column content

--- src/components/LandingFooter/LandingFooter.js
+++ src/components/LandingFooter/LandingFooter.js
@@ -19,39 +19,43 @@ export default class LandingFooter extends Component {
           <img className="footer-logo" src={logo} />
         </div>
         <div className="footer-col" style={{flex: 3}}>
-          <h3>{str.contact}</h3>
           <div>
-            <a className="footer-link" href={config.emailLink}>
-              <span>{str.email}</span>
-            </a>
-          </div>
-          <div>
-            <a className="footer-link" href={config.facebookLink}>
-              <span>Facebook</span>
-            </a>
-          </div>
-          <div>
-            <a className="footer-link" href={config.twitterLink}>
-              <span>Twitter</span>
-            </a>
-          </div>
-          <div>
-            <a className="footer-link" href={config.instagramLink}>
-              <span>Instagram</span>
-            </a>
+            <h3>{str.contact}</h3>
+            <div>
+              <a className="footer-link" href={config.emailLink}>
+                <span>{str.email}</span>
+              </a>
+            </div>
+            <div>
+              <a className="footer-link" href={config.facebookLink}>
+                <span>Facebook</span>
+              </a>
+            </div>
+            <div>
+              <a className="footer-link" href={config.twitterLink}>
+                <span>Twitter</span>
+              </a>
+            </div>
+            <div>
+              <a className="footer-link" href={config.instagramLink}>
+                <span>Instagram</span>
+              </a>
+            </div>
           </div>
         </div>
         <div className="footer-col" style={{flex: 3}}>
-          <h3>{str.other}</h3>
           <div>
-            <a className="footer-link" href={config.privacyPolicyLink}>
-              <span>{str.privacyPolicy}</span>
-            </a>
-          </div>
-          <div>
-            {/*<a className="footer-link" href={config.termsAndConditionsLink}>
-              <span>Terms & Conditions</span>
-            </a>*/}
+            <h3>{str.other}</h3>
+            <div>
+              <a className="footer-link" href={config.privacyPolicyLink}>
+                <span>{str.privacyPolicy}</span>
+              </a>
+            </div>
+            <div>
+              {/*<a className="footer-link" href={config.termsAndConditionsLink}>
+                <span>Terms & Conditions</span>
+              </a>*/}
+            </div>
           </div>
         </div>
       </div>
